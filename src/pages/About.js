9793675fcf56319b6f897bafc94ff0cd438bdd44
Jessica <simply.me05@hotmail.com>
2021-06-11 10:46:05 -0400

    changes to display in about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,18 +6,18 @@ import Col from "../components/Col";
 
 function About() {
   return (
-    <div>
+    <div class="auto">
       <Hero backgroundImage="https://img.freepik.com/free-vector/programming-code-coding-hacker-background-programming-code-icon-made-with-binary-code_127544-815.jpg?size=626&ext=jpg">
         <h1>Jessica Gibbs</h1>
       </Hero>
       <Container style={{ marginTop: 30 }}>
-        <Row>
-          <Col size="md-12">
+        <Row size="auto">
+          <Col size="auto">
             <h1>Welcome to my page!!</h1>
           </Col>
         </Row>
-        <Row>
-          <Col size="md-9">
+        <Row size="auto">
+          <Col size="auto">
             <p>
               I appreciate you stopping by for a visit. My name is Jessica Gibbs
               and I currently work for a non-profit agency where are focus is
@@ -35,7 +35,7 @@ function About() {
               forward.
             </p>
           </Col>
-          <Col size="md-3">
+          <Col size="auto">
             <img src="/Me.jpg" width="300" height="300" alt="" />
           </Col>
         </Row>
